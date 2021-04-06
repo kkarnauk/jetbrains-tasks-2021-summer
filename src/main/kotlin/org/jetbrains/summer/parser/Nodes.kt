b7c66eb4345ca0ee6d3d0b08678ab3889ab42a07
Kirill <bitikit@gmail.com>
2021-04-06 23:51:27 +0300

    Implement nodes and remove unnecessary ones

--- src/main/kotlin/org/jetbrains/summer/parser/Nodes.kt
+++ src/main/kotlin/org/jetbrains/summer/parser/Nodes.kt
@@ -1,53 +1,30 @@
 package org.jetbrains.summer.parser
 
 abstract class Node {
-    abstract fun evaluate(): Int
+    abstract fun evaluate(varValues: HashMap<String, Int>): Int
 }
 
-class ProgramNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
+class ConstantNode(private val value: Int) : Node() {
+    override fun evaluate(varValues: HashMap<String, Int>): Int = value
+}
+
+class IdentifierNode(private val value: String) : Node() {
+    override fun evaluate(varValues: HashMap<String, Int>): Int {
+        if (varValues.containsKey(value)) {
+            return varValues.getValue(value)
+        } else {
+            throw ParameterNotFoundException()
+        }
     }
 }
 
-class ConstantNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
-    }
+class BinaryExpressionNode(private val left: Node, private val right: Node,
+                           private val operation: (Int, Int) -> Int) : Node() {
+    override fun evaluate(varValues: HashMap<String, Int>): Int =
+        operation(left.evaluate(varValues), right.evaluate(varValues))
 }
 
-class IdentifierNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
-    }
-}
-
-class BinaryExpressionNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
-    }
-}
-
-class CallExpressionNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
-    }
-}
-
-class IfExpressionNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
-    }
-}
-
-class ParameterListNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
-    }
-}
-
-class FunctionDefinitionNode : Node() {
-    override fun evaluate(): Int {
-        TODO("Not yet implemented")
-    }
+class IfExpressionNode(private val condition: Node, private val ifTrue: Node, private val ifFalse: Node) : Node() {
+    override fun evaluate(varValues: HashMap<String, Int>): Int =
+        if (condition.evaluate(varValues) != 0) ifTrue.evaluate(varValues) else ifFalse.evaluate(varValues)
 }
