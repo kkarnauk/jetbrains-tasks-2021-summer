4a651373928486f165f7ca8d63e61ddb21653590
Kirill <bitikit@gmail.com>
2021-04-06 23:53:02 +0300

    Implement parser for function defenition

--- src/main/kotlin/org/jetbrains/summer/parser/Parser.kt
+++ src/main/kotlin/org/jetbrains/summer/parser/Parser.kt
@@ -9,8 +9,59 @@ class Parser private constructor(program: String) {
     }
 
     private val lexer = Lexer(program)
+    private val functions: HashMap<String, FunctionDefinition> = HashMap()
 
     private fun parseProgram(): Node {
         TODO("Not yet implemented")
     }
+
+    private fun parseFunctionDefinition() {
+        getNextExpectedLexema(Lexema.Identifier)
+        val funcName = lexer.currentIdentifier
+        val params = parseParametersList()
+        getNextExpectedLexema(Lexema.EqualsSign)
+        getNextExpectedLexema(Lexema.LeftBrace)
+        val expression = parseExpression()
+
+        if (functions.containsKey(funcName)) {
+            throw MultipleFunctionDefinitionException()
+        }
+
+        functions[funcName] = FunctionDefinition(params, expression)
+
+        getNextExpectedLexema(Lexema.RightBrace)
+    }
+
+    private fun parseExpression(): Node {
+        TODO("Not yet implemented")
+    }
+
+    private fun parseParametersList(): List<String> {
+        getNextExpectedLexema(Lexema.LeftParen)
+
+        val params = ArrayList<String>()
+        while (true) {
+            getNextExpectedLexema(Lexema.Identifier)
+            params.add(lexer.currentIdentifier)
+            if (lexer.nextLexema() != Lexema.Comma) {
+                break
+            }
+        }
+
+        if (lexer.currentLexema != Lexema.RightParen) {
+            throw IllegalSyntaxException()
+        }
+
+        return params
+    }
+
+    private fun getNextExpectedLexema(expectedLexema: Lexema): Lexema {
+        val lexema = lexer.nextLexema()
+        if (lexema != expectedLexema) {
+            throw IllegalSyntaxException()
+        }
+        return lexema
+    }
+
+    private data class FunctionDefinition(val args: List<String>, val expression: Node)
 }
