de6b277f21f1c3d0941693cb9448b05c0e632492
Kirill <bitikit@gmail.com>
2021-04-06 21:20:49 +0300

    Implement lexer

---
+++ src/main/kotlin/org/jetbrains/summer/parser/Lexema.kt
@@ -0,0 +1,25 @@
+package org.jetbrains.summer.parser
+
+enum class Lexema {
+    Identifier,
+    Number,
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    PercentSign,
+    GreaterSign,
+    LessSign,
+    EqualsSign,
+    Comma,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    LeftParen,
+    RightParen,
+    QuestionMark,
+    Semicolon,
+    EOL,
+    EOF
+}
---
+++ src/main/kotlin/org/jetbrains/summer/parser/Lexer.kt
@@ -0,0 +1,99 @@
+package org.jetbrains.summer.parser
+
+import java.io.StringReader
+import java.lang.Exception
+import java.lang.StringBuilder
+
+class Lexer(program: String) {
+    class IllegalSyntaxException : Exception()
+
+    private val reader: StringReader = StringReader(program)
+
+    var currentLexema: Lexema = Lexema.EOF
+        private set
+    var currentNumber: Int = 0
+        private set
+        get() {
+            if (currentLexema != Lexema.Number) {
+                throw IllegalStateException("Cannot get number value: current lexema is not Number")
+            }
+            return field
+        }
+    var currentIdentifier: String = ""
+        private set
+        get() {
+            if (currentLexema != Lexema.Identifier) {
+                throw IllegalStateException("Cannot get identifier value: current lexema is not Identifier")
+            }
+            return field
+        }
+
+    private var currentChar = '\u0000'
+
+    private fun nextChar() {
+        val value = reader.read()
+        currentChar = if (value == -1) {
+            '\u0000'
+        } else {
+            value.toChar()
+        }
+    }
+
+    fun nextLexema(): Lexema {
+        var lexema: Lexema = Lexema.EOF
+        when (currentChar) {
+            '\u0000' -> {
+                currentLexema = Lexema.EOF
+                return currentLexema
+            }
+            '+' -> lexema = Lexema.Plus
+            '-' -> lexema = Lexema.Minus
+            '*' -> lexema = Lexema.Asterisk
+            '/' -> lexema = Lexema.Slash
+            '%' -> lexema = Lexema.PercentSign
+            '>' -> lexema = Lexema.GreaterSign
+            '<' -> lexema = Lexema.LessSign
+            '=' -> lexema = Lexema.EqualsSign
+            ',' -> lexema = Lexema.Comma
+            ':' -> lexema = Lexema.Semicolon
+            '(' -> lexema = Lexema.LeftParen
+            ')' -> lexema = Lexema.RightParen
+            '[' -> lexema = Lexema.LeftBracket
+            ']' -> lexema = Lexema.RightBracket
+            '{' -> lexema = Lexema.LeftBrace
+            '}' -> lexema = Lexema.RightBrace
+            '?' -> lexema = Lexema.QuestionMark
+            '\n' -> lexema = Lexema.EOL
+        }
+
+        when {
+            lexema != Lexema.EOF -> {
+                currentLexema = lexema
+                nextChar()
+            }
+            currentChar.isDigit() -> {
+                val numberBuilder = StringBuilder()
+                while (currentChar.isDigit()) {
+                    numberBuilder.append(currentChar)
+                    nextChar()
+                }
+                currentNumber = numberBuilder.toString().toInt()
+                currentLexema = Lexema.Number
+            }
+            currentChar.isLetter() -> {
+                val identifierBuilder = StringBuilder()
+                while (currentChar.isLetter()) {
+                    identifierBuilder.append(currentChar)
+                    nextChar()
+                }
+                currentIdentifier = identifierBuilder.toString()
+                currentLexema = Lexema.Identifier
+            }
+            else -> {
+                throw IllegalSyntaxException()
+            }
+        }
+
+        return currentLexema
+    }
+}
